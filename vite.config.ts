891d21559828efb460610e991991776cc2c253f1
Aish Maheshwari <155080572+stillaish@users.noreply.github.com>
2025-09-10 11:17:33 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      input: './index.html'
+    }
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
